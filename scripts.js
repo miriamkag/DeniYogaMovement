3b5a2c50e32f95dc73e3afc0bea1cba0d9e77832
miriamkag <64091804+miriamkag@users.noreply.github.com>
2021-01-17 12:38:32 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+console.log("Hello Miriam")
+let variable=false
+if(variable===true){console.log("Write this to console log")}
+
+let a=5
+if(a/5==2){console.log(true)}
+else {console.log(false)
+}
+
+
+let bookcount = 10
+
+for (let i=0; i < (bookcount*2); i=i+2) {
+  console.log(i)
+}
+
+let printnumbers = 20
+for (let i=0; i<=printnumbers; i++){
+  console.log(i)
+}
+
+printnumbers(){}
