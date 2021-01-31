487011ef79c9a2aa6bb941858a40a4c3dac2920e
miriamkag <64091804+miriamkag@users.noreply.github.com>
2021-01-31 20:32:27 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 if(isset($_POST['submit'])){
     $to = "mgiljanova@gmail.com"; // this is your Email address
@@ -17,3 +18,24 @@ if(isset($_POST['submit'])){
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
 ?>
+=======
+<?php
+if(isset($_POST['submit'])){
+    $to = "mgiljanova@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+>>>>>>> 0add476aeea84d8be84216cf24dc805dcf8c76b4
